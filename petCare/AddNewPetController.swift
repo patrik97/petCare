3d66eb16b14b7d22e0e1994ba00dae9be52ef665
Patrik Pluhar <patrik.pluhar97@gmail.com>
2020-09-07 19:42:54 +0200

    implementation of pickerView species in AddNewPetController

--- petCare/AddNewPetController.swift
+++ petCare/AddNewPetController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class AddNewPetController: UIViewController {
+    let species = Species.allCases.map { $0.rawValue }
+    var currentSpecies: Species = Species.dog
+    @IBOutlet weak var speciesPickerView: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        speciesPickerView.delegate = self
+        speciesPickerView.dataSource = self
     }
     
     @IBAction func cancelButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Cancel without saving?", message: "It may cause loss data", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { action in self.presentingViewController?.dismiss(animated: true, completion: nil) }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Save", style: .default, handler: { action in self.save() }))
+        self.present(alert, animated: true)
+    }
+    
+    func save() {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension AddNewPetController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return species.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return species[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        currentSpecies = Species.allCases[row]
     }
 }
